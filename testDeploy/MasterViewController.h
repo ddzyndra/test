d370b2fb00aeb0a2ea43fc850093070219ffb2e6
“Dariy <ddzyndra@softjourn.com>
2014-11-11 16:21:52 +0200

    Initial Commit

---
+++ testDeploy/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  testDeploy
+//
+//  Created by Dariy Dzyndra on 11/11/14.
+//  Copyright (c) 2014 SoftJourn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
